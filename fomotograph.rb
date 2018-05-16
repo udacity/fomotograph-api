b37e99eedcc9505c636b8d0965fbeb24d74901ed
peiyanz <peiyan@udacity.com>
2018-05-16 16:47:44 -0700

    add: erasurez endpoint

--- fomotograph.rb
+++ fomotograph.rb
@@ -8,3 +8,11 @@ get '/products.json' do
   content_type :json
   data_hash.to_json
 end
+
+
+data = JSON.parse('{"serviceName": "fomotograph-api","personalData": false}')
+
+get '/erasurez' do
+  content_type :json
+  data.to_json
+end
